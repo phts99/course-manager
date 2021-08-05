ee4627204db8105864c0184b4ee221d0749ad974
Pedro Trindade <phts99@gmail.com>
2021-08-05 09:07:20 -0300

    Aplied new ID retriever and save methods to course info component

--- src/app/courses/course-info.component.ts
+++ src/app/courses/course-info.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
     templateUrl: './course-info.component.html'
@@ -7,11 +9,17 @@ import { ActivatedRoute } from "@angular/router";
 
 export class CourseInfoComponent implements OnInit {
 
-    courseId!: number;
+    course!: Course;
 
-    constructor (private activatedRoute: ActivatedRoute) {}
+    constructor (private activatedRoute: ActivatedRoute, private courseService: CourseService) {}
 
     ngOnInit(): void {
-        this.courseId = Number(this.activatedRoute.snapshot.paramMap.get('id'));
+        // non-null assertion operator -> ! at the end of the method operation
+        // assures the ts compiler that the outcome will not be null even though it seems like it could be
+        this.course = this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id')!);
+    }
+
+    save(): void {
+        this.courseService.save(this.course);
     }
 }
